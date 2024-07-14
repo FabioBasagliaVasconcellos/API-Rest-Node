4968776ebab6ef6a1d6da36f6eb7aad5825c8084
FabioBasagliaVasconcellos <fbcvasconcellos@gmail.com>
2024-07-14 14:16:21 +0100

    initial commit

--- src/database.ts
+++ src/database.ts
@@ -1,11 +1,33 @@
-import { knex as setupKnex } from "knex";
+//import { knex as setupKnex, Knex } from 'knex'
+//import { env } from './env'
 
-export const config = {
-    client: "sqlite",
-    connection: {
-        filename: "./tmp/app.db",
-    },
-    useNullAsDefault: trues,
+//export const config: Knex.Config = {
+  //  client: 'sqlite',
+    //connection: {
+    // filename: env.DATABASE_URL
+    //},
+    //useNullAsDefault: true,
+    //migrations: {
+      //  extension: '.ts',
+        //directory: './db/migrations',
+    //}
+//}
+
+//export const knex = setupKnex(config)
+
+import knex, { Knex } from 'knex';
+import { env } from './env';
+
+export const config: Knex.Config = {
+  client: 'sqlite',
+  connection: {
+    filename: env.DATABASE_URL,
+  },
+  useNullAsDefault: true,
+  migrations: {
+    extension: 'ts',
+    directory: './migrations',
+  },
 };
 
-export const knex = setupKnex(config);
+export const db = knex(config);
